2d06ddeeb90bf0524d0fc889f36f2cb60ff54db6
Niki Yoshiuchi <nyoshiuchi@gmail.com>
2024-05-23 20:40:18 -0700

    Populating the menu with items

--- systray.c
+++ systray.c
@@ -1,8 +1,29 @@
 #include <gtk/gtk.h>
 
+void callback(GtkWidget*, gpointer);
 void resize_systray(GtkStatusIcon*, gint, gpointer);
 void on_click(GtkStatusIcon*, gpointer);
 GtkMenu* create_menu();
+GtkWidget* create_menu_item(char*, char*);
+
+typedef struct {
+  char* name;
+  char* command;
+} MenuItem;
+
+static const MenuItem menu_items[] = {
+  { "Lock", "slock" },
+  { "Logout", "pkill dwm" },
+  { "Suspend", "systemctl suspend" },
+  { "Hibernate", "systemctl hibernate" },
+  { "Restart", "systemctl reboot" },
+  { "Shutdown", "sudo -A shutdown -h now" },
+};
+
+void callback(GtkWidget* widget, gpointer command) {
+  GError *error = NULL;
+  g_spawn_command_line_async(command, &error);
+}
 
 void resize_systray(GtkStatusIcon* icon, gint _size, gpointer dummy) {
   gint size = gtk_status_icon_get_size (icon);
@@ -21,14 +42,26 @@ void on_click(GtkStatusIcon* icon, gpointer menu) {
 
 GtkMenu* create_menu() {
   GtkMenu* menu = GTK_MENU (gtk_menu_new());
-  GtkWidget *shutdown = gtk_menu_item_new_with_label("Shutdown");
-  GtkWidget *restart = gtk_menu_item_new_with_label("Restart");
-  gtk_widget_show(shutdown);
-  gtk_widget_show(restart);
-  gtk_menu_shell_append(GTK_MENU_SHELL (menu), restart);
-  gtk_menu_shell_append(GTK_MENU_SHELL (menu), shutdown);
+  size_t num_menu_items = sizeof(menu_items) / sizeof(MenuItem);
+  for (int i = 0; i < num_menu_items; ++i) {
+    GtkWidget *menu_item =
+      create_menu_item(menu_items[i].name, menu_items[i].command);
+    gtk_menu_shell_append(GTK_MENU_SHELL (menu), menu_item);
+  }
 
   return menu;
+}
+
+GtkWidget* create_menu_item(char* name, char* command) {
+  GtkWidget *menu_item = gtk_menu_item_new_with_label(name);
+  g_signal_connect(
+      G_OBJECT (menu_item),
+      "activate",
+      G_CALLBACK (callback),
+      command);
+  gtk_widget_show(menu_item);
+
+  return menu_item;
 }
 
 void create_systray() {
